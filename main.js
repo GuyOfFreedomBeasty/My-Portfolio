0dd5823cbb1044a357e420e658029f25ad7bc212
GuyOfFreedomBeasty <guyoffreedoms@gmail.com>
2024-05-26 18:23:59 +0400

    finished about page

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const selectionChoises = document.getElementsByClassName("selection-choise")
+const infoSection = document.getElementsByClassName("info-section")
 
 function SwitchSelection(){
-    console.log("hi");
+    for (var i = 0; i < selectionChoises.length; i++){
+        selectionChoises[i].classList.remove("selected-section-choise")
+    }
+    for (var i = 0; i < infoSection.length; i++){
+        infoSection[i].classList.remove("selected-section")
+    }
+
+    const selectionToDisplayName = this.getAttribute("data-section-name")
+    const sectionToDisplay = document.getElementById(selectionToDisplayName)
+    
+    this.classList.add("selected-section-choise")
+    sectionToDisplay.classList.add("selected-section")
 }
 
 for (var i = 0; i < selectionChoises.length; i++){
-    selectionChoises[i].addEventListener("click", selectionChoises)
+    selectionChoises[i].addEventListener("click", SwitchSelection)
 }
