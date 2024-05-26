4704689bf2a2b2676584c92ea40affc46cb8d2a5
GuyOfFreedomBeasty <guyoffreedoms@gmail.com>
2024-05-26 18:10:17 +0400

    in middle of doing the selection chooser

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// nothing here yet
+const selectionChoises = document.getElementsByClassName("selection-choise")
+
+function SwitchSelection(){
+    console.log("hi");
+}
+
+for (var i = 0; i < selectionChoises.length; i++){
+    selectionChoises[i].addEventListener("click", selectionChoises)
+}
