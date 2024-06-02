be8d1e23e4046349ab73569451bf45c5c2b6ac41
GuyOfFreedomBeasty <guyoffreedoms@gmail.com>
2024-06-02 19:07:39 +0400

    Responsive design finished. Tbh i need to commit more cuz there is kinda less point in github

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const selectionChoises = document.getElementsByClassName("selection-choise")
 const infoSection = document.getElementsByClassName("info-section")
 const easterEggText = document.getElementById("easter-egg-text")
-const soundToPlay = new Audio('audio/hee-hee.mp3');
+const soundToPlay = new Audio('audio/hee-hee.mp3')
+
+const navUl = document.getElementsByClassName("navigation-ul")[0]
+const openBarMenu = document.getElementsByClassName("fa-bars")[0]
+const closeBarMenu = document.getElementsByClassName("fa-x")[0]
 
 function SwitchSelection(){
     for (var i = 0; i < selectionChoises.length; i++){
@@ -24,4 +28,13 @@ for (var i = 0; i < selectionChoises.length; i++){
 
 easterEggText.addEventListener("click", function(){
     soundToPlay.play();
+})
+
+openBarMenu.addEventListener("click", function(){
+    navUl.style.right = "0" 
+    console.log("made the right 0")
+})
+
+closeBarMenu.addEventListener("click", function(){
+    navUl.style.right = "-200px"
 })
